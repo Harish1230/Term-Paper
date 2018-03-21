adde3f228fd81c42c30cbce8f07ad0b9ec5c2a80
Harish <hharish270@gmail.com>
2018-03-21 17:53:19 +0530

    Add files via upload

---
+++ mq135/mq135.ino
@@ -0,0 +1,16 @@
+void setup() {
+  pinMode(A1,INPUT);
+  Serial.begin(9600);
+}
+
+void loop() {
+  int value=analogRead(A0);
+  Serial.print(value);
+  Serial.println(" ppm");
+  /*if(value>=10 && value<=300)
+    Serial.println("ammonia is detected");
+    Serial.println("alcohol is detected");
+  if(value>=10 && value<=1000)
+    Serial.println("benzene is detected");
+  delay(2000);*/
+}
---
+++ mq7/mq7.ino
@@ -0,0 +1,14 @@
+void setup() {
+  pinMode(A1,INPUT);
+  Serial.begin(9600);
+
+}
+
+void loop() {
+  int al=analogRead(A1);
+  float av=0.0048828*al;
+  float ap=(2000/5)*av;
+  Serial.print(ap);
+  Serial.println(" ppm");
+  delay(2000);
+}
